511443d4f73078d347059f7adfff89b50f34adec
Sumedh <sumedhmhaske310503@gmail.com>
2025-07-09 20:50:23 +0530

    Enhance Signup functionality by integrating InputBox for user input and adding API call for signup

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -3,23 +3,57 @@ import { SubHeading } from "../components/SubHeading";
 import { InputBox } from "../components/InputBox";
 import { Button } from "../components/Button";
 import { BottomWarning } from "../components/BottomWarning";
-
-//
-//
+import { useState } from "react";
+import axios from "axios";
 
 export const Signup = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="bg-slate-300 h-screen flex justify-center">
       <div className="flex flex-col justify-center">
         <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
           <Heading label={"Sign up"} />
           <SubHeading label={"Enter you information to create an account"} />
-          <InputBox label={"First Name"} placeholder="John" />
-          <InputBox label={"Last Name"} placeholder="Doe" />
-          <InputBox label={"sumedh@gmail.com"} placeholder={"Email"} />
-          <InputBox label={"Password"} placeholder={"123456"} />
+          <InputBox
+            onChange={(e) => setFirstName(e.target.value)}
+            label={"First Name"}
+            placeholder="John"
+          />
+          <InputBox
+            onChange={(e) => setLastName(e.target.value)}
+            label={"Last Name"}
+            placeholder="Doe"
+          />
+          <InputBox
+            onChange={(e) => setUsername(e.target.value)}
+            label={"sumedh@gmail.com"}
+            placeholder={"Email"}
+          />
+          <InputBox
+            onChange={(e) => setPassword(e.target.value)}
+            label={"Password"}
+            placeholder={"123456"}
+          />
           <div className="pt-4">
-            <Button label={"Sign up"} />
+            <Button
+              onClick={async () => {
+                const res = await axios.post(
+                  "http://localhost:3000/api/v1/user/signup",
+                  {
+                    username,
+                    firstName,
+                    lastName,
+                    password,
+                  },
+                );
+                localStorage.setItem("token", res.data.token);
+              }}
+              label={"Sign up"}
+            />
           </div>
           <BottomWarning
             label={"Already have an account"}
